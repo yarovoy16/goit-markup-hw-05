20ae72ab6436e166eadc7700c1ae334499a46f8b
yarovoy16 <yarovoy.od@gmail.com>
2023-02-20 21:10:59 +0100

    modal

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@
         const refs = {
 openModalBtn: document.querySelector('[data-modal-open]'),
 closeModalBtn: document.querySelector('[data-modal-close]'),
-backdrop: document.querySelector('[data-backdrop]'),
+backdrop: document.querySelector('[data-modal]'),
         };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
